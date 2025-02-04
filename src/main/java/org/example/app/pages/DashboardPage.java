e9a7636e08678e44b29d1a48cbe2bf35703bdddb
QuocHoang94 <39055816+QuocHoang94@users.noreply.github.com>
2025-02-04 22:59:12 +0700

    assiment NAB

--- src/main/java/org/example/app/pages/DashboardPage.java
+++ src/main/java/org/example/app/pages/DashboardPage.java
@@ -1,26 +1,92 @@
 package org.example.app.pages;
 
-import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import dev.failsafe.internal.util.Assert;
 
 public class DashboardPage {
-    WebDriver driver;
+    private WebDriver driver;
 
-    WebElement navbar_brand_logo = driver.findElement(By.xpath("//a[contains(@class,'navbar__brand')]"));
-    WebElement navbar_tutorial_menu = driver.findElement(By.xpath("//a[contains(@class,'navbar__tutorial-menu')]"));
-    WebElement navbar_links_home = driver.findElement(By.xpath("//div[contains(@class,'navbar__links')]//ul/li/a[text()='Home']"));
-    WebElement navbar_links_seleniumTraining = driver.findElement(By.xpath("//div[contains(@class,'navbar__links')]//ul/li/a[text()='Selenium Training']"));
-    WebElement navbar_links_demoSite = driver.findElement(By.xpath("//div[contains(@class,'navbar__links')]//ul/li/a[text()='Demo Site']"));
-    WebElement navbar_links_about = driver.findElement(By.xpath("//div[contains(@class,'navbar__links')]//ul/li/a[text()='About']"));
-    WebElement navbar_links_search = driver.findElement(By.xpath("//div[contains(@class,'navbar__links-and-search')]//div[contains(@class,'navbar__search')]"));
-
-    public DashboardPage(WebDriver driver){
+    public DashboardPage(WebDriver driver) {
+        if (driver == null) {
+            throw new IllegalArgumentException("WebDriver không được null!");
+        }
         this.driver = driver;
     }
+    // WebElement navbar_brand_logo = driver.findElement(By.xpath("//a[contains(@class,'navbar__brand')]"));
+    // WebElement navbar_tutorial_menu = driver.findElement(By.xpath("//a[contains(@class,'navbar__tutorial-menu')]"));
+    // WebElement navbar_links_home = driver.findElement(By.xpath("//div[contains(@class,'navbar__links')]//ul/li/a[text()='Home']"));
+    // WebElement navbar_links_seleniumTraining = driver.findElement(By.xpath("//div[contains(@class,'navbar__links')]//ul/li/a[text()='Selenium Training']"));
+    // WebElement navbar_links_demoSite = driver.findElement(By.xpath("//div[contains(@class,'navbar__links')]//ul/li/a[text()='Demo Site']"));
+    // WebElement navbar_links_about = driver.findElement(By.xpath("//div[contains(@class,'navbar__links')]//ul/li/a[text()='About']"));
+    // WebElement navbar_links_search = driver.findElement(By.xpath("//div[contains(@class,'navbar__links-and-search')]//div[contains(@class,'navbar__search')]"));
+
+    private WebElement getLoginEmail() {
+        return driver.findElement(By.xpath("//input[@id='Email']"));
+    }
+
+    private WebElement getLoginPassword() {
+        return driver.findElement(By.xpath("//input[@id='Password']"));
+    }
+
+    private WebElement getLoginButton() {
+        return driver.findElement(By.xpath("//input[@class='button-1 login-button']"));
+    }
+
+    private WebElement getAccountlogin(){
+        return driver.findElement(By.xpath("//div[@class='header-links']/ul/li/a[@class='account']"));
+    }
+
+    public WebElement divTextUnsuccessful(){
+        return driver.findElement(By.xpath("//div[@class='validation-summary-errors']"));
+    }
+
+    public WebElement spanTextUnsuccessful(){
+        return driver.findElement(By.xpath("//div[@class='validation-summary-errors']/span"));
+    }
+
+    public WebElement liTextCredentialsIncorrect(){
+        return driver.findElement(By.xpath("//div[@class='validation-summary-errors']/ul/li"));
+    }
+
+    public WebElement inputRememberme(){
+        return driver.findElement(By.xpath("//input[@id='RememberMe']"));
+    }
+
+    public WebElement focusedElement(){
+        return driver.switchTo().activeElement();
+    }
+
     public void clickon_brand_logo() {
-        navbar_brand_logo.click();
+        // navbar_brand_logo.click();
+    }
+
+    public void login(String email, String password) {
+        getLoginEmail().sendKeys(email);
+        getLoginPassword().sendKeys(password);
+        getLoginButton().click();
+    }
+
+    public void loginByTab() {
+        getLoginEmail().sendKeys(Keys.TAB);
+        getLoginPassword().sendKeys(Keys.TAB);
+    }
+
+    public void loginByEnter() {
+        getLoginEmail().sendKeys(Keys.ENTER);
+    }
+
+    public boolean verifyAccountLoginSuccess(){
+        return getAccountlogin().isDisplayed();
+    }
+
+    public boolean verifyAccountLoginfail(){
+        return divTextUnsuccessful().isDisplayed();
+    }
+
+    public String getText(WebElement element){
+        return element.getText();
     }
 }
--- src/test/java/org/example/app/SimpleTest3.java
+++ src/test/java/org/example/app/SimpleTest3.java
@@ -1,26 +1,91 @@
 package org.example.app;
 
 import org.example.app.pages.DashboardPage;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
 public class SimpleTest3 {
-        WebDriver driver ;
-        @Test
-        public void f() {
+    private WebDriver driver;
+    String baseUrl = "https://demowebshop.tricentis.com/login";
+    @BeforeMethod
+    public void setUp() {
+        // Sử dụng WebDriverManager thay vì khởi tạo trực tiếp
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
 
-            String baseUrl = "https://www.toolsqa.com/";
-            driver = new ChromeDriver();
-            driver.navigate().to(baseUrl);
-            DashboardPage dashboardPage = new DashboardPage(driver);
-            dashboardPage.clickon_brand_logo();
+    @Test(description = "Verify successful login and account display")
+    public void submitformSuccess() {
+        driver.navigate().to(baseUrl);
+        // Kiểm tra driver trước khi truyền vào DashboardPage
+        if (driver == null) {
+            throw new IllegalStateException("WebDriver chưa được khởi tạo!");
         }
+        DashboardPage dashboardPage = new DashboardPage(driver);
+        dashboardPage.login("nashtechhoang@gmail.com","123321456");
+        Assert.assertTrue(dashboardPage.verifyAccountLoginSuccess());
+    }
 
-        @BeforeMethod
-        public void beforeMethod() {
-            driver.close();
+    @Test(description ="Verify failed login")
+    public void submitFormFail(){
+        driver.navigate().to(baseUrl);
+        if (driver == null) {
+            throw new IllegalStateException("WebDriver chưa được khởi tạo!");
         }
+        DashboardPage dashboardPage = new DashboardPage(driver);
+        dashboardPage.login("nashtechhoang@gmail.com"," ");
+        Assert.assertTrue(dashboardPage.verifyAccountLoginfail());
+        Assert.assertEquals(dashboardPage.getText(dashboardPage.spanTextUnsuccessful()), "Login was unsuccessful. Please correct the errors and try again.");
+        Assert.assertEquals(dashboardPage.getText(dashboardPage.liTextCredentialsIncorrect()), "The credentials provided are incorrect");
+    }
+
+    @Test(description ="Verify field blank")
+    public void submitFieldBlank(){
+        driver.navigate().to(baseUrl);
+        if (driver == null) {
+            throw new IllegalStateException("WebDriver chưa được khởi tạo!");
+        }
+        DashboardPage dashboardPage = new DashboardPage(driver);
+        dashboardPage.login(" "," ");;
+        Assert.assertTrue(dashboardPage.verifyAccountLoginfail());
+        Assert.assertEquals(dashboardPage.getText(dashboardPage.spanTextUnsuccessful()), "Login was unsuccessful. Please correct the errors and try again.");
+        Assert.assertEquals(dashboardPage.getText(dashboardPage.liTextCredentialsIncorrect()), "No customer account found");
+    }
+
+    @Test(description ="Verify Tab is working")
+    public void verifyTabIsWorking(){
+        driver.navigate().to(baseUrl);
+        if (driver == null) {
+            throw new IllegalStateException("WebDriver chưa được khởi tạo!");
+        }
+        DashboardPage dashboardPage = new DashboardPage(driver);
+        dashboardPage.loginByTab();
+        Assert.assertEquals(dashboardPage.inputRememberme(), dashboardPage.focusedElement());
+    }
+
+    @Test(description ="Verify Enter is working")
+    public void verifyEnterIsWorking(){
+        driver.navigate().to(baseUrl);
+        if (driver == null) {
+            throw new IllegalStateException("WebDriver chưa được khởi tạo!");
+        }
+        DashboardPage dashboardPage = new DashboardPage(driver);
+        dashboardPage.loginByEnter();
+        Assert.assertEquals(dashboardPage.getText(dashboardPage.spanTextUnsuccessful()), "Login was unsuccessful. Please correct the errors and try again.");
+        Assert.assertEquals(dashboardPage.getText(dashboardPage.liTextCredentialsIncorrect()), "No customer account found");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
